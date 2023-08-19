9fea6746ded0d3028d19232f09b40d80a7bbe5db
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-08-19 01:22:17 -0400

    Index has now been updated

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -4,5 +4,8 @@ export * from "./ApplicationCommandLocalization";
 export * from "./ApplicationCommandType";
 export * from "./ApplicationCommandOptions";
 export * from "./ApplicationCommandOptionsChoice";
+export * from "./ApplicationCommandOptionType";
+export * from "./ChannelType";
+export * from "./ChannelTypeEnum";
 export * from "./Routes";
 export * from "./request/typings/index";
