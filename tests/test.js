e22ae4b9429a02d2f5efb0ed8337aa1393ac1c13
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-08-20 01:09:28 -0400

    chore: typedoc

---
+++ tests/test.js
@@ -0,0 +1,47 @@
+const {
+  WaifuSlash,
+  Command,
+  ApplicationCommandOptionType,
+  ApplicationCommandType,
+} = require("../dist");
+const config = require("./config.json");
+const waifu = new WaifuSlash({
+  botID: config.botID,
+  botToken: config.botToken,
+});
+
+const blepCommand = new Command()
+.setName("animal")
+.setType(ApplicationCommandOptionType.SUB_COMMAND)
+.setDescription("Send a random adorable animal photo")
+.setOptions([
+    {
+        name: "animal",
+        description: "The type of animal",
+        type: ApplicationCommandType.MESSAGE,
+        required: true,
+        choices: [
+            {
+                name: "Dog",
+                value: "animal_dog"
+            },
+            {
+                name: "Cat",
+                value: "animal_cat"
+            },
+            {
+                name: "Penguin",
+                value: "animal_penguin"
+            }
+        ]
+    },
+])
+async function test() {
+  await waifu.editGuildCommand({
+    guildId: "845605014663856158",
+    commandId: "1142661941777682432",
+    command: blepCommand.toJSON()
+  });
+}
+
+test();
